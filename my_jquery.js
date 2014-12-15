edda922b044870f0854479acdebf6df0be54f081
Evelyn Tio <tiomyster@gmail.com>
2014-12-14 23:32:30 -0800

    yo

---
+++ my_jquery.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+
+  $('#san-x').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/san-x_on.jpg');
+
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/san-x_cover.jpg');
+    }
+  });
+
+   $('#robotfed').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/Robot_Federer_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/Robot_Federer_cover.jpg');
+    }
+  });
+
+  $('#spray').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/spray_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/spray_cover.jpg');
+    }
+  });
+
+  $('#function').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/function_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/function_cover.jpg');
+    }
+  });
+
+  $('#lathe').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/lathe_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/lathe_cover.jpg');
+    }
+  });
+
+    $('#dlab').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/dlab_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/dlab_cover.jpg');
+    }
+  });
+
+     $('#gummy').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/gummy_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/gummy_cover.jpg');
+    }
+  });
+
+   $('#crayon').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/crayon_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/crayon_cover.jpg');
+    }
+  });   
+
+    $('#yoyo').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/yoyo_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/yoyo_cover.jpg');
+    }
+  });
+
+ $('#techfair').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/techfair_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/techfair_cover.jpg');
+    }
+  });
+
+ $('#toy').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/toy_on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/toy_cover.jpg');
+    }
+  });
+
+ $('#spatty').on({
+    'mouseover' : function() {
+      $(this).attr('src','./images/007on.jpg');
+    },
+    mouseout : function() {
+  $(this).attr('src','./images/007_cover.jpg');
+    }
+  });
+
+});
